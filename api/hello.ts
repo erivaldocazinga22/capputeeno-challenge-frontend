784cc78f41aa0f8a459919ec878215245a245d81
erivaldocazinga22 <cazarthdesogner@gmail.com>
2024-12-28 13:43:25 +0100

    feat: serveless funcition hello 4.4

--- api/hello.ts
+++ api/hello.ts
@@ -1,5 +1,11 @@
 import { VercelRequest, VercelResponse } from '@vercel/node';
 
 export default function handler(req: VercelRequest, res: VercelResponse) {
+  if (req.method === "GET") {
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader("Access-Control-Allow-Methods", "GET");
+    res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+    return res.json({ message: 'Serverless function is running!' });
+  }
   res.status(200).json({ message: 'Hello from Vercel Serverless Function!' });
 }
