fb6a7c8214c3a59a30f261c1cd71463a869b2760
erivaldocazinga22 <cazarthdesogner@gmail.com>
2024-12-28 13:27:08 +0100

    feat: serveless funcition hello 3

--- vite.config.ts
+++ vite.config.ts
@@ -9,16 +9,17 @@ export default defineConfig({
       "@": path.resolve(__dirname, "./src"), // Atalho para paths absolutos
     },
   },
-  build: {
-    outDir: "dist", // Diretório de saída para o build
-    rollupOptions: {
-      input: {
-        main: path.resolve(__dirname, "index.html"), // Entrada principal
+  server: {
+    port: 3000, // Porta do servidor de desenvolvimento
+    open: true, // Abrir o navegador automaticamente
+    proxy: {
+      // Proxy para redirecionar chamadas para o JSON Server
+      "/api": {
+        target: "http://localhost:5000", // Onde o JSON Server estará rodando
+        changeOrigin: true,
+        secure: false,
+        rewrite: (path) => path.replace(/^\/api/, ""), // Remove /api da URL antes de passar para o JSON Server
       },
     },
-  },
-  server: {
-    port: 3000, // Porta do servidor local
-    open: true, // Abrir o navegador automaticamente
   },
 });
