30c22e67ea4ca61a239a21747836161e004c9603
erivaldocazinga22 <cazarthdesogner@gmail.com>
2024-12-28 13:18:30 +0100

    feat: serveless funcition hello 2

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,24 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import path from "path";
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
-      "@": "/src",
+      "@": path.resolve(__dirname, "./src"), // Atalho para paths absolutos
     },
   },
-})
+  build: {
+    outDir: "dist", // Diretório de saída para o build
+    rollupOptions: {
+      input: {
+        main: path.resolve(__dirname, "index.html"), // Entrada principal
+      },
+    },
+  },
+  server: {
+    port: 3000, // Porta do servidor local
+    open: true, // Abrir o navegador automaticamente
+  },
+});
