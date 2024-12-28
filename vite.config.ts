86a71bce5c51149ba6cecfe7fd97e2eff430e28f
erivaldocazinga22 <cazarthdesogner@gmail.com>
2024-12-28 13:36:20 +0100

    feat: serveless funcition hello 4.1

--- vite.config.ts
+++ vite.config.ts
@@ -1,25 +1,12 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import path from "path";
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "./src"), // Atalho para paths absolutos
+      "@": "/src",
     },
   },
-  server: {
-    port: 3000, // Porta do servidor de desenvolvimento
-    open: true, // Abrir o navegador automaticamente
-    proxy: {
-      // Proxy para redirecionar chamadas para o JSON Server
-      "/api": {
-        target: "http://localhost:5000", // Onde o JSON Server estará rodando
-        changeOrigin: true,
-        secure: false,
-        rewrite: (path) => path.replace(/^\/api/, ""), // Remove /api da URL antes de passar para o JSON Server
-      },
-    },
-  },
-});
+})
