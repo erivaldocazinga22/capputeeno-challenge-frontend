edc64eefc03b206806f5623537c745979ceb8867
erivaldocazinga22 <cazarthdesogner@gmail.com>
2024-12-28 12:36:42 +0100

    feat: ajuste json-server serveless 1.3

--- src/api/db.ts
+++ src/api/db.ts
@@ -1,4 +1,5 @@
 import jsonServer from "json-server";
+import cors from "cors";
 import { VercelRequest, VercelResponse } from "@vercel/node";
 import path from "node:path";
 
@@ -6,6 +7,9 @@ const server = jsonServer.create();
 const router = jsonServer.router(path.join(__dirname, "..", "..", "db.json"));
 const middlewares = jsonServer.defaults();
 
+server.use(cors({
+    origin: "*"
+}))
 server.use(middlewares);
 server.use(router);
 
--- src/lib/axios.config.ts
+++ src/lib/axios.config.ts
@@ -2,5 +2,5 @@ import Axios from "axios";
 import { env } from "./env.config";
 
 export const axios = Axios.create({
-    baseURL: env.VITE_ROCKETSEAT_URL
+    baseURL: env.VITE_ROCKETSEAT_URL || "https://capputeeno-challenge-frontend.vercel.app/api"
 });
