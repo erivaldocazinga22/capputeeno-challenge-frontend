6a6f0942fef3b8d665aa0485734154e2572b6f53
erivaldocazinga22 <cazarthdesogner@gmail.com>
2024-12-28 13:47:27 +0100

    feat: elimine serverless

--- src/lib/axios.config.ts
+++ src/lib/axios.config.ts
@@ -1,6 +1,6 @@
 import Axios from "axios";
-// import { env } from "./env.config";
+import { env } from "./env.config";
 
-export const axios = Axios.create(/* {
-    baseURL: env.VITE_ROCKETSEAT_URL || "https://capputeeno-challenge-frontend.vercel.app/api"
-} */);
+export const axios = Axios.create({
+    baseURL: env.VITE_ROCKETSEAT_URL
+});
