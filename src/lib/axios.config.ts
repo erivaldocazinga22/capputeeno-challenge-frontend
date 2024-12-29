0006e063a7399fb2bb14a08a81da57c1925fa91f
Erivaldo Malebo Cazinga <136964420+erivaldocazinga22@users.noreply.github.com>
2024-12-29 12:22:40 +0100

    Update axios.config.ts

--- src/lib/axios.config.ts
+++ src/lib/axios.config.ts
@@ -2,5 +2,5 @@ import Axios from "axios";
 import { env } from "./env.config";
 
 export const axios = Axios.create({
-    baseURL: env.VITE_ROCKETSEAT_URL
+    baseURL: env.VITE_ROCKETSEAT_URL + "/api/capputeeno"
 });
